e1307a53a37e5e25c0ebf4bba65497b5a390b05a
zhengbo <zhengbo@pv25.com>
2017-04-05 17:23:09 +0800

    修复ｂｕｇ

--- fcache.php
+++ fcache.php
@@ -8,26 +8,22 @@
 class Fcache
 {
     private $fhandle = null;
-
     public function __construct($fpath)
     {
         if (!$fpath) {
             throw new Exception("文件路径不能为空");
         }
-
         if (!file_exists($fpath)) {
             $this->fhandle = fopen($fpath, "w+");
         } else {
             $this->fhandle = fopen($fpath, "r+");
         }
-
         if (!$this->fhandle) {
             throw new Exception("文件创建失败");
         }
         $this->init();
         flock($this->fhandle, LOCK_EX);
     }
-
     public function init()
     {
         if (!fgetc($this->fhandle)) {
@@ -35,7 +31,6 @@ class Fcache
             fwrite($this->fhandle, pack("cLLx440000", 1, 9, 440009));
         }
     }
-
     public function get($key)
     {
         $meta = $this->find($key);
@@ -45,10 +40,8 @@ class Fcache
                 return trim(fread($this->fhandle, 128));
             }
         }
-
         return "";
     }
-
     public function set($key, $value, $expire = 0)
     {
         $expire = $expire ? $expire + time() : 0;
@@ -57,14 +50,14 @@ class Fcache
         $key = $key . str_repeat("", 32 - strlen($key));
         $value = strlen($value) > 128 ? substr($value, 0, 128) : $value;
         $value = $value . str_repeat("", 128 - strlen($value));
-       // var_dump($meta);
+        // var_dump($meta);
         // 已存在
         if ($meta[0] == "exist") {
             fseek($this->fhandle, $meta[1]['kstart']);
             fwrite($this->fhandle, pack("a32lll", $key, $meta[1]['start'], $meta[1]['end'], $expire));
             fseek($this->fhandle, $meta[1]['start']);
             fwrite($this->fhandle, pack("a128", $value));
-            flush($this->fhandle);
+            fflush($this->fhandle);
             return true;
         }
         //过去的key
@@ -73,10 +66,9 @@ class Fcache
             fwrite($this->fhandle, pack("a32lll", $key, $meta[1]['start'], $meta[1]['end'], $expire));
             fseek($this->fhandle, $meta[1]['start']);
             fwrite($this->fhandle, pack("a128", $value));
-            flush($this->fhandle);
+            fflush($this->fhandle);
             return true;
         }
-
         //插入
         fseek($this->fhandle, $meta[1]['kend']);
         fwrite($this->fhandle, pack("a32lll", $key, $meta[1]['vend'], $meta[1]['vend'] + 128, $expire));
@@ -86,10 +78,9 @@ class Fcache
         fwrite($this->fhandle, pack("L", $meta[1]['kend'] + 44));
         fseek($this->fhandle, 5);
         fwrite($this->fhandle, pack("L", $meta[1]['vend'] + 128));
-        flush($this->fhandle);
+        fflush($this->fhandle);
         return true;
     }
-
     private function find($key)
     {
         fseek($this->fhandle, 0);
@@ -98,24 +89,19 @@ class Fcache
         if (!$metas) {
             return false;
         }
-
         $hmetas = unpack("ctag/Lkend/Lvend", $metas);
-
         //遍历数据所有数据
         $startIndex = 9;
         $keysMetas = [];
-
         for ($i = $startIndex; $i < strlen($metas); $i = $i + 44) {
             $tmp = substr($metas, $i, 44);
             $meta = @unpack("a32key/lstart/lend/lexpire", $tmp);
-
             if ($meta && trim($meta['key']) != "") {
                 // var_dump($meta);
                 $meta['kstart'] = $i;
                 $keysMetas[] = $meta;
             }
         }
-
         foreach ($keysMetas as $met) {
             if (!isset($met['key'])) {
                 continue;
@@ -128,7 +114,6 @@ class Fcache
                 return ['exist', $met, $hmetas];
             }
         }
-
         //查找过期的key
         foreach ($keysMetas as $met) {
             if (!isset($met['key'])) {
@@ -140,7 +125,6 @@ class Fcache
         }
         return [false, $hmetas];
     }
-
     public function del($key)
     {
         $meta = $this->find($key);
@@ -149,10 +133,9 @@ class Fcache
             fwrite($this->fhandle, pack("a128", ""));
             fseek($this->fhandle, $meta[1]['kstart']);
             fwrite($this->fhandle, pack("a32lll", $key, $meta[1]['start'], $meta[1]['end'], -1));
-            flush($this->fhandle);
+            fflush($this->fhandle);
         }
     }
-
     public function __destruct()
     {
         if ($this->fhandle) {
